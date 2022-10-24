2366bbb9449815498aae13cef6d6fccb0e965239
dominicod <dominicodonnell99@gmail.com>
2022-10-24 13:51:13 -0500

    Feat: create logic for logging in a User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,17 +24,23 @@ class UsersController < ApplicationController
     end
   end
 
-  def login_form
-
-  end
+  def login_form; end
 
   def login_user
-
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}"
+      redirect_to user_path(user.id)
+    else
+      flash[:error] = 'Error: Incorrect Credentials'
+      render :login_form
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email.downcase, :password, :password_confirmation)
   end
 end
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -3,37 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe 'User | Login' do
+  let!(:user) { User.create!(name: 'dominic', email: 'dominicod@gmail.com', password: 'password_1', password_confirmation: 'password_1') }
+
   describe 'When I visit /login;'
   it 'I see a form to login' do
     visit login_path
 
-    expect(page).to have_field(:user_email)
-    expect(page).to have_field(:user_password)
-    expect(page).to have_button('Login')
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+    expect(page).to have_button('Log In')
   end
 
   context 'Happy Path' do
     it 'should take the user to their dashboard page after they login' do
-      User.create!(name: 'dominic', email: 'dominicod@gmail.com', password: 'password_1', password_confirmation: 'password_1')
       visit login_path
 
-      fill_in :user_email, with: 'dominicod@gmail.com'
-      fill_in :user_password, with: 'password_1'
-      click_on 'Login'
+      fill_in :email, with: user.email
+      fill_in :password, with: 'password_1'
+      click_on 'Log In'
 
       expect(page.current_path).to eq user_path(User.last)
-      expect(page).to have_content("Welcome #{User.last.name}")
+      expect(page).to have_content("Welcome, #{User.last.name}")
     end
   end
 
   context 'Sad Path' do
     it 'should return user to login form if information is incorrect' do
-      User.create!(name: 'dominic', email: 'dominicod@gmail.com', password: 'password_1', password_confirmation: 'password_1')
       visit login_path
 
-      fill_in :user_email, with: 'dominicod@gmail.com'
-      fill_in :user_password, with: 'password_10000'
-      click_on 'Login'
+      fill_in :email, with: user.email
+      fill_in :password, with: 'password_10000'
+      click_on 'Log In'
 
       expect(page.current_path).to eq login_path
       expect(page).to have_content('Error: Incorrect Credentials')
