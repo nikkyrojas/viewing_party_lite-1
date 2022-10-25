fb5902d199f6222e2b32dce613eb68312e8432cb
dominicod <dominicodonnell99@gmail.com>
2022-10-25 14:05:13 -0500

    Feat: add session logic to create and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{@user.name}!"
       redirect_to "/users/#{@user.id}"
     else
