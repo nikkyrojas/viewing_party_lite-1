4a7b4a1d2f8c612edf2135ef946ed1a48d25e63b
dominicod <dominicodonnell99@gmail.com>
2022-10-25 15:22:27 -0500

    Test: update landing_page and movies specs to use session

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe 'Movie | Index' do
   describe 'As a user when I visit the Movies Results Page', :vcr do
     before(:each) do
       @user1 = User.create!(name: 'jojo binks', email: 'jojo_binks@gmail.com', password: 'password_1', password_confirmation: 'password_1')
-      visit user_discover_index_path(@user1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
+      visit user_discover_index_path
     end
     context 'Happy Path' do
       it 'has a button to discover top rated movies' do
         click_button 'Discover Top Rated Movies'
-        expect(current_path).to eq(user_movies_path(@user1))
+        expect(current_path).to eq(user_movies_path)
         expect(page).to have_content('The Shawshank Redemption')
       end
       it 'has a search function that displays matching movies' do
@@ -22,11 +24,11 @@ RSpec.describe 'Movie | Index' do
         expect(page).to have_content('Impossible Things')
       end
       it 'has a button to return to discover page' do
-        visit user_movies_url(@user1)
+        visit user_movies_url
 
         expect(page).to have_button('Discover Movies')
         click_button 'Discover Movies'
-        expect(page.current_path).to eq(user_discover_index_path(@user1))
+        expect(page.current_path).to eq(user_discover_index_path)
       end
 
       it 'has text field to search key words then redirects to matching list' do
@@ -35,7 +37,7 @@ RSpec.describe 'Movie | Index' do
         fill_in :search, with: 'Shawshank Redemption'
         click_on 'Search'
         expect(page).to have_content('Shawshank Redemption')
-        expect(page.current_path).to eq(user_movies_path(@user1))
+        expect(page.current_path).to eq(user_movies_path)
       end
     end
     context 'Sad Path' do
@@ -45,7 +47,7 @@ RSpec.describe 'Movie | Index' do
         within('.container') do
           expect(page).to have_content('Error: Incorrect information entered')
         end
-        expect(page.current_path).to eq(user_discover_index_path(@user1))
+        expect(page.current_path).to eq(user_discover_index_path)
       end
     end
   end
